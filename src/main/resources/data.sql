7bbfa64ddb20181fdb8f140aebef90ca8f8b5a2b
FedericoQuarin <federicoquarin4@gmail.com>
2023-07-31 11:36:43 -0300

    Bind saveButton

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,7 +9,9 @@ INSERT INTO "COMPANY" (ID, VERSION, NAME) VALUES
 (7, 1, 'Avaya Inc.'),
 (8, 1, 'Laboratory Corporation of America Holdings'),
 (9, 1, 'AutoZone, Inc.'),
-(10, 1, 'Linens ''n Things Inc.');   
+(10, 1, 'Linens ''n Things Inc.'),
+(61, 1, 'Flowing Code SA'),
+(62, 1, 'Playcom SA');
 INSERT INTO "CONTACT" (ID, VERSION, EMAIL,FIRST_NAME,LAST_NAME,COMPANY_ID,STATUS_ID) VALUES
 (11, 1, 'eula.lane@jigrormo.ye', 'Eula', 'Lane', 8, 1),
 (12, 1, 'barry.rodriquez@zun.mm', 'Barry', 'Rodriquez', 7, 5),
